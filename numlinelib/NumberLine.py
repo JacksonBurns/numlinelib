af78a88e57a057bd395eb758f4286980d0effb19
JacksonBurns <JacksonBurns@users.noreply.github.com>
2021-08-19 22:42:58 -0400

    add method stubs

--- numlinelib/NumberLine.py
+++ numlinelib/NumberLine.py
@@ -2,5 +2,67 @@ import matplotlib.pyplot as plt
 
 
 class NumberLine:
-    def __init__(self):
+    def __init__(self, *args):
+
+        self.fig = plt.Figure()
+        # return the instance of the class
+        if not args:
+            self.points = []
+        # plot points and return the figure
+        else:
+            self.set_points(args)
+            self.show()
         return
+
+    def _sanitize_input(self, points):
+        """Return a 1D array.
+
+        Args:
+            points (iterable): Points to be sanitized.
+        """
+        # check for pandas, numpy, list, etc. and dimensionality
+        return points
+
+    def add_points(self, points):
+        """Adds points to the plot.
+
+        Args:
+            points (list): Points to be added.
+        """
+        self._sanitize_input(points)
+        self.points.append(points)
+        return
+
+    def set_points(self, points):
+        """Remove existing points and set new points.
+
+        Args:
+            points (list): Points to be added.
+        """
+        self.clear_points()
+        self.add_points(points)
+        return
+
+    def clear_points(self):
+        """Removes all points on the plot."""
+        self.points = []
+        # clear points from the number line
+        return
+
+    def clear_figure(self):
+        """Return to an empty figure."""
+        return
+
+    def set_max(self, max):
+        return
+
+    def set_min(self, min):
+        return
+
+    def set_ticks(self, ticks):
+        return
+
+    def show(self):
+        """Show the number line, return the figure."""
+        plt.show()
+        return self.fig
--- test/test_numlinelib.py
+++ test/test_numlinelib.py
@@ -1,6 +1,21 @@
 import unittest
+from numlinelib.NumberLine import NumberLine as NumLine
 
 
 class Testnumlinelib(unittest.TestCase):
-    def test_init(self):
-        self.assertTrue(True)
+    """Test the methods of the NumberLine class.
+
+    Args:
+        unittest (TestCase): Boilerplate for unittest.
+    """
+
+    def test_empty_initialization(self):
+        """Ensure NumberLine can be initialized with no args."""
+        numline = NumLine()
+        self.assertIsInstance(numline, NumLine)
+
+    def test_initialization(self):
+        """Ensure NumberLine can be initialized with args."""
+        points = [1, 2, 3]
+        numline = NumLine(points)
+        self.assertEqual(points, numline.points[0])
